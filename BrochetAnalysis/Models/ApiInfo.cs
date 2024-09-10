af6660daf5c12e1ecd6fc65f1892dd436dc0d092
Lukas Holecy <lukasholecy@gmail.com>
2024-09-11 00:18:30 +0200

    Fixed compilation errors. Not doing anything but compiles

--- BrochetAnalysis/Models/ApiInfo.cs
+++ BrochetAnalysis/Models/ApiInfo.cs
@@ -1,15 +1,17 @@
-namespace Holecy.Services.Analysis.Models
+namespace Holecy.Services.Analysis.Models;
+
+using System.Globalization;
+using System.Resources;
+
+public class ApiInfo
 {
-    public class ApiInfo
-    {
-        private static readonly ResourceManager ResourceManager = new ResourceManager("Brochet.Services.Analysis.Resources", typeof(ApiInfo).Assembly);
-        
-        public string Name => ResourceManager.GetString("Name", CultureInfo.CurrentCulture);
+    private static readonly ResourceManager ResourceManager = new ResourceManager("Brochet.Services.Analysis.Resources", typeof(ApiInfo).Assembly);
+    
+    public string Name => ResourceManager.GetString("Name", CultureInfo.CurrentCulture)!;
 
-        public string Version => "1"
+    public string Version => "1";
 
-        public string AssemblyVersion => typeof(ApiInfo).Assembly.GetName().Version.ToString();
+    public string AssemblyVersion => typeof(ApiInfo).Assembly.GetName().Version?.ToString()!;
 
-        public string Description => ResourceManager.GetString("Description", CultureInfo.CurrentCulture);
-    }
+    public string Description => ResourceManager.GetString("Description", CultureInfo.CurrentCulture)!;
 }
