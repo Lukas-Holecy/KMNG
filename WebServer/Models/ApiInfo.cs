1036e918ef90b361720a94d76d03fea3ea5dacd2
Lukas Holecy <lukas@brochet.co.uk>
2024-09-11 20:15:55 +0200

    Added directory props files with default properties to include solution wide stylecop.json. Relates to #4
    Using new directory files.
    
    - Includes centraly located packages versions.
    - Added central stylecop.json
    
    All projects use central stylecop.json
    Closes #4

--- WebServer/Models/ApiInfo.cs
+++ WebServer/Models/ApiInfo.cs
@@ -1,12 +1,22 @@
+// <copyright file="ApiInfo.cs" company="Lukas Holecy">
+//     Copyright (c) Lukas Holecy. All rights reserved.
+// </copyright>
+
 namespace Holecy.Services.Models;
 
 using System.Globalization;
 using System.Resources;
 
+/// <summary>
+/// Represents information about this RestAPI.
+/// </summary>
 public class ApiInfo
 {
     private static readonly ResourceManager ResourceManager = new ResourceManager("Holecy.Services.Resources.AapiInfoResources", typeof(ApiInfo).Assembly);
-        
+
+    /// <summary>
+    /// Gets the name of the property.
+    /// </summary>
     public string Name => ResourceManager.GetString("Name", CultureInfo.CurrentCulture)!;
 
     public string Version => "1";
