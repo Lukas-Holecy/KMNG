8d58de9400d623ae381dc5018e7a46539fb16791
Lukas Holecy <lukasholecy@gmail.com>
2024-09-11 11:00:42 +0200

    Fixed a few warnings

--- WebServer/Models/ApiInfo.cs
+++ WebServer/Models/ApiInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 public class ApiInfo
 {
     private static readonly ResourceManager ResourceManager = new ResourceManager("Brochet.Services.Analysis.Resources", typeof(ApiInfo).Assembly);
-    
+        
     public string Name => ResourceManager.GetString("Name", CultureInfo.CurrentCulture)!;
 
     public string Version => "1";
