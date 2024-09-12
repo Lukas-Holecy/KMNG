531f5afa44649a49986e9d04d5dd4787cb19aae5
Lukas Holecy <lukasholecy@gmail.com>
2024-09-12 19:23:51 +0200

    Enabled swagger (#16)
    Also swagger page automatically opens when the project is run.

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -26,7 +26,11 @@ public class Program
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
         }
 
         app.UseHttpsRedirection();
